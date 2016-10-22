97a30c3418dcda626e408b9352ec630f0e8a50ff
GeorgeSalu <george.salu10@gmail.com>
2016-10-22 15:17:40 -0200

    componente input - oneButton

---
+++ src/main/java/org/primefaces/showcase/view/input/SelectOneView.java
@@ -0,0 +1,18 @@
+package org.primefaces.showcase.view.input;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class SelectOneView {
+
+	private String option;
+
+	public String getOption() {
+		return option;
+	}
+
+	public void setOption(String option) {
+		this.option = option;
+	}
+
+}
