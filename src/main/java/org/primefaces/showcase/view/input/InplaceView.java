0658322966e55c2e11d2a3ef20748589e2efe1ce
GeorgeSalu <george.salu10@gmail.com>
2016-10-23 15:43:47 -0200

    componente input - knob

--- src/main/java/org/primefaces/showcase/view/input/InplaceView.java
+++ src/main/java/org/primefaces/showcase/view/input/InplaceView.java
@@ -7,6 +7,10 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 public class InplaceView implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -243777578091732829L;
 	private String text = "Primefaces";
 
 	public String getText() {
