b1bc872a15181a3069399b485d5dc12460450be9
GeorgeSalu <george.salu10@gmail.com>
2016-10-29 18:43:15 -0200

    componente input - comandLink

---
+++ src/main/java/org/primefaces/showcase/view/button/ButtonView.java
@@ -0,0 +1,20 @@
+package org.primefaces.showcase.view.button;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean
+public class ButtonView {
+
+	public void buttonAction(ActionEvent actionEvent){
+		addMessage("Bem vindo ao primefaces");
+	}
+	
+	public void addMessage(String summary){
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+	
+}
