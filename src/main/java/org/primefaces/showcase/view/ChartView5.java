a3b85324d25e637bfb882aff0a151f9482b0efc3
GeorgeSalu <george.salu10@gmail.com>
2016-11-27 14:09:41 -0200

    componente charts - combined

---
+++ src/main/java/org/primefaces/showcase/view/ChartView5.java
@@ -0,0 +1,63 @@
+package org.primefaces.showcase.view;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.BarChartSeries;
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.LineChartSeries;
+
+@ManagedBean
+public class ChartView5 implements Serializable {
+
+	private CartesianChartModel combinedModel;
+
+	@PostConstruct
+	public void init() {
+		createCombinedModel();
+	}
+
+	public CartesianChartModel getCombinedModel() {
+		return combinedModel;
+	}
+
+	private void createCombinedModel() {
+		combinedModel = new BarChartModel();
+
+		BarChartSeries boys = new BarChartSeries();
+		boys.setLabel("Boys");
+
+		boys.set("2004", 120);
+		boys.set("2005", 100);
+		boys.set("2006", 44);
+		boys.set("2007", 150);
+		boys.set("2008", 25);
+
+		LineChartSeries girls = new LineChartSeries();
+		girls.setLabel("Girls");
+
+		girls.set("2004", 52);
+		girls.set("2005", 60);
+		girls.set("2006", 110);
+		girls.set("2007", 135);
+		girls.set("2008", 120);
+
+		combinedModel.addSeries(boys);
+		combinedModel.addSeries(girls);
+
+		combinedModel.setTitle("Bar and Line");
+		combinedModel.setLegendPosition("ne");
+		combinedModel.setMouseoverHighlight(false);
+		combinedModel.setShowDatatip(false);
+		combinedModel.setShowPointLabels(true);
+		Axis yAxis = combinedModel.getAxis(AxisType.Y);
+		yAxis.setMin(0);
+		yAxis.setMax(200);
+	}
+
+}
